9140fd16ba2b9d1ac06e4c7f9b0fc94bcb909585
牛海军 <niuhaijun@100tal.com>
2020-04-05 22:08:17 +0800

    format

--- kafka/src/test/java/com/niu/kafka/KafkaApplicationTests.java
+++ kafka/src/test/java/com/niu/kafka/KafkaApplicationTests.java
@@ -11,6 +11,7 @@ public class KafkaApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
--- rocketmq/src/main/java/com/niu/RocketmqApplication.java
+++ rocketmq/src/main/java/com/niu/RocketmqApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RocketmqApplication {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    SpringApplication.run(RocketmqApplication.class, args);
-  }
+		SpringApplication.run(RocketmqApplication.class, args);
+	}
 
 }
